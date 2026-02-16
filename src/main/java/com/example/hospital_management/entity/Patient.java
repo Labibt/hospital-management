2b12c19e5b3ea1630e1eb18bb305bf726c186890
Labibt <labibt42@gmail.com>
2026-02-16 17:09:51 +0600

    @table+@column

--- src/main/java/com/example/hospital_management/entity/Patient.java
+++ src/main/java/com/example/hospital_management/entity/Patient.java
@@ -1,31 +1,44 @@
 package com.example.hospital_management.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @ToString
+@Table(name = "patient_tbl",
+        uniqueConstraints = {
+            @UniqueConstraint(name = "unique_name_birthdate", columnNames = {"name", "birth_date"})
+        },
+        indexes = {
+            @Index(name = "idx_birth_date", columnList = "birth_date"),
+        }
+)
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     private LocalDate birthDate;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
     private String gender;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
